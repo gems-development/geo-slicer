fd6a9a1e658a3f8a3c3a6b7727b239ee89030a0f
max1219 <bescenniy.2003@mail.ru>
2023-10-26 11:34:36 +0300

    Added Conventers and LineStringToLineSegments

---
+++ GeoSlicer.Utils/Conventers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+
+namespace GeoSlicer.Utils;
+
+public static class Converters
+{
+    public static IEnumerable<LineSegment> LineStringToLineSegments(LineString lineString)
+    {
+        foreach ((Coordinate, Coordinate) pair in lineString.Coordinates.Pairwise())
+        {
+            yield return new LineSegment(pair.Item1, pair.Item2);
+        }
+    }
+}
