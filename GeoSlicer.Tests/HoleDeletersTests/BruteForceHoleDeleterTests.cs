01255869bfe03afdfba24ec7dc80510502e637d9
max1219 <bescenniy.2003@mail.ru>
2023-11-09 08:57:46 +0300

    Added BruteForceHoleDeleterTests

---
+++ GeoSlicer.Tests/HoleDeletersTests/BruteForceHoleDeleterTests.cs
@@ -0,0 +1,25 @@
+﻿using GeoSlicer.HoleDeleters;
+using NetTopologySuite.Geometries;
+
+namespace GeoSlicer.Tests.HoleDeletersTests;
+
+public class BruteForceHoleDeleterTests
+{
+    [Theory]
+    [InlineData("HoleDeletersTests/TestData/sample.geojson")]
+    private void SavesAllPoints(string fileName)
+    {
+        Polygon polygon = GeoJsonFileService.GeoJsonFileService.ReadGeometryFromFile<Polygon>(fileName);
+        BruteForceHoleDeleter holeDeleter = new BruteForceHoleDeleter();
+        HashSet<Coordinate> expected = new HashSet<Coordinate>(polygon.Coordinates);
+        foreach (LinearRing hole in polygon.Holes)
+        {
+            expected.UnionWith(hole.Coordinates);
+        }
+
+        LinearRing result = holeDeleter.DeleteHoles(polygon);
+        HashSet<Coordinate> actual = new HashSet<Coordinate>(result.Coordinates);
+        
+        Assert.Equal(expected, actual);
+    }
+}
