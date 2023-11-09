07828cf93d6da665f9a06cafb17d2893c1794b22
max1219 <bescenniy.2003@mail.ru>
2023-11-09 10:03:49 +0300

    Added bench for BruteForceHoleDeleter

---
+++ GeoSlicer.Benchmark/Benchmarks/HoleDeletersBench.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+using GeoSlicer.HoleDeleters;
+using NetTopologySuite.Geometries;
+
+namespace GeoSlicer.Benchmark.Benchmarks;
+
+public class HoleDeletersBench
+{
+    private static readonly Polygon With3AHoles =
+        GeoJsonFileService.GeoJsonFileService.ReadGeometryFromFile<Polygon>("GeoSlicer.Benchmark/BenchData/with_3a_holes.geojson");
+
+    [Benchmark]
+    public void TestBruteForceHoleDeleter()
+    {
+        BruteForceHoleDeleter bruteForceHoleDeleter = new BruteForceHoleDeleter();
+        bruteForceHoleDeleter.DeleteHoles(With3AHoles);
+    }
+}
