70a5c4d32332fd855471cd63a52feaa90fc5a33f
max1219 <bescenniy.2003@mail.ru>
2023-11-16 13:08:18 +0300

    Added CoordinateComparators : interface, epsilon and ref

---
+++ GeoSlicer.Utils/Intersectors/CoordinateComparators/ICoordinateComparator.cs
@@ -0,0 +1,8 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace GeoSlicer.Utils.Intersectors.CoordinateComparators;
+
+public interface ICoordinateComparator
+{
+    bool IsEquals(Coordinate a, Coordinate b);
+}
---
+++ GeoSlicer.Utils/Intersectors/CoordinateComparators/RefCoordinateComparator.cs
@@ -0,0 +1,11 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace GeoSlicer.Utils.Intersectors.CoordinateComparators;
+
+public class RefCoordinateComparator : ICoordinateComparator
+{
+    public bool IsEquals(Coordinate a, Coordinate b)
+    {
+        return ReferenceEquals(a, b);
+    }
+}
