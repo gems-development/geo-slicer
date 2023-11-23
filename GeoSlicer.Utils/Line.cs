98878a29c783623761bf7f8a21918ebc8aea4823
max1219 <bescenniy.2003@mail.ru>
2023-11-23 08:09:18 +0300

    Refactored Line fields naming and added constructor

--- GeoSlicer.Utils/Line.cs
+++ GeoSlicer.Utils/Line.cs
@@ -4,6 +4,12 @@ namespace GeoSlicer.Utils;
 
 public struct Line
 {
-    public Coordinate a;
-    public Coordinate b;
+    public Coordinate A;
+    public Coordinate B;
+
+    public Line(Coordinate a, Coordinate b)
+    {
+        A = a;
+        B = b;
+    }
 }
