d2dfaba0f38f9aa88390d57bb60cff9d950c8fa2
max1219 <bescenniy.2003@mail.ru>
2024-02-14 12:50:41 +0400

    Added epsilons to AccuracyCalculator

--- GeoSlicer.Utils/AccuracyCalculation/AccuracyCalculator.cs
+++ GeoSlicer.Utils/AccuracyCalculation/AccuracyCalculator.cs
@@ -1,15 +1,22 @@
-﻿namespace GeoSlicer.Utils.AccuracyCalculation;
+﻿using System;
+
+namespace GeoSlicer.Utils.AccuracyCalculation;
 
 public class AccuracyCalculator
 {
     public int SumAccuracy => _maxAccuracy;
     public int MulAccuracy(int multiplyCount) => _maxAccuracy * multiplyCount;
 
+    public double SumEpsilon => _epsilon;
+    public double MulEpsilon(int multiplyCount) => Math.Pow(_epsilon, multiplyCount);
+
     private readonly int _maxAccuracy;
+    private readonly double _epsilon;
 
 
     public AccuracyCalculator(int maxAccuracy)
     {
         _maxAccuracy = maxAccuracy;
+        _epsilon = Math.Pow(0.1, _maxAccuracy);
     }
 }
