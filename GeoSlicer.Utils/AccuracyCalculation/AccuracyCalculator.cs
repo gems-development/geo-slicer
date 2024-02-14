2b39e58f6e912c045fbcfb9c8abf1d675ef21182
max1219 <bescenniy.2003@mail.ru>
2024-02-14 11:56:47 +0400

    Added AccuracyCalculator

---
+++ GeoSlicer.Utils/AccuracyCalculation/AccuracyCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace GeoSlicer.Utils.AccuracyCalculation;
+
+public class AccuracyCalculator
+{
+    public int SumAccuracy => _maxAccuracy;
+    public int MulAccuracy(int multiplyCount) => _maxAccuracy * multiplyCount;
+
+    private readonly int _maxAccuracy;
+
+
+    public AccuracyCalculator(int maxAccuracy)
+    {
+        _maxAccuracy = maxAccuracy;
+    }
+}
