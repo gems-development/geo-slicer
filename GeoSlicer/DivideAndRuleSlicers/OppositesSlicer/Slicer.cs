019d7c2bf48bb2f95104b08df170454b86c5b26b
max1219 <bescenniy.2003@mail.ru>
2024-04-17 10:36:03 +0400

    Added some mb usefull lines for keep

--- GeoSlicer/DivideAndRuleSlicers/OppositesSlicer/Slicer.cs
+++ GeoSlicer/DivideAndRuleSlicers/OppositesSlicer/Slicer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using GeoSlicer.Utils;
 using GeoSlicer.Utils.Intersectors;
 using NetTopologySuite.Geometries;
 
@@ -9,26 +11,75 @@ public class Slicer
 {
     private readonly OppositesSlicerHelper _oppositesSlicerHelper;
     private readonly LinesIntersector _linesIntersector;
+    private readonly LineService _lineService;
+    private readonly int _maxPointsCount;
 
-    public Slicer(OppositesSlicerHelper oppositesSlicerHelper, LinesIntersector linesIntersector)
+    public Slicer(OppositesSlicerHelper oppositesSlicerHelper, LinesIntersector linesIntersector,
+        LineService lineService, int maxPointsCount)
     {
         _oppositesSlicerHelper = oppositesSlicerHelper;
         _linesIntersector = linesIntersector;
+        _lineService = lineService;
+        _maxPointsCount = maxPointsCount;
     }
 
 
-    public IEnumerable<MultiPolygon> Slice(Polygon polygon)
+    public IEnumerable<LinearRing> Slice(LinearRing input, int maxPointsCount = -1)
     {
-        int oppositesIndex = Utils.GetNearestOppositesInner(polygon.Shell);
-        Console.WriteLine(oppositesIndex);
-        throw new NotImplementedException();
+        LinkedList<LinearRing> result = new LinkedList<LinearRing>();
+
+        if (input.Count <= maxPointsCount)
+        {
+            result.AddLast(input);
+            return result;
+        }
+
+        Queue<LinearRing> queue = new Queue<LinearRing>();
+        queue.Enqueue(input);
+
+        while (queue.Count != 0)
+        {
+            LinearRing current = queue.Dequeue();
+            int oppositesIndex = Utils.GetNearestOppositesInner(current);
+            IEnumerable<LinearRing> sliced = SliceByLine(
+                current,
+                current.GetCoordinateN(oppositesIndex),
+                current.GetCoordinateN((oppositesIndex + current.Count / 2) % current.Count));
+            foreach (LinearRing ring in sliced)
+            {
+                if (ring.Count <= maxPointsCount)
+                {
+                    result.AddLast(ring);
+                }
+                else
+                {
+                    queue.Enqueue(ring);
+                }
+            }
+        }
+
+        return result;
     }
-    
-    // todo Сделать соединение без создания новых точек
+
+    // todo Сделать соединение без создания новых точек по возможности (мб принимать опцию на это)
     // todo Вынести в отдельный класс, мб пригодится извне
-    private IEnumerable<Polygon> SliceByLine(Polygon polygon, Coordinate a, Coordinate b)
+    // todo Подумать, есть ли смысл возвращать 2 коллекции с разделенными данными вместо одной
+    private IEnumerable<LinearRing> SliceByLine(LinearRing input, Coordinate a, Coordinate b)
     {
+        LinkedList<LinearRing> result = new LinkedList<LinearRing>();
+        Coordinate[] coordinates = input.Coordinates;
         
-        // Мб не пересечение, а направление от линии
+        LinkedList<Coordinate> current
+
+        // todo учесть вырезы колец
+        // todo Задуматься о быстром объединении LinkedList
+        int sign = Math.Sign(_lineService.VectorProduct(
+            a, b, 
+            coordinates[0], coordinates[1]));
+
+        for (int i = 1; i < coordinates.Length - 1; i++)
+        {
+            
+        }
     }
 }
