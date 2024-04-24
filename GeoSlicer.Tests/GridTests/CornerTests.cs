1f0bf92c3c3b1c1df354993b0f49fb418640a082
SuchuMichail <michail_suchu@mail.ru>
2024-04-24 14:55:48 +0600

    Added class for tests

---
+++ GeoSlicer.Tests/GridTests/CornerTests.cs
@@ -0,0 +1,19 @@
+﻿using GeoSlicer.GridSlicer.Helpers;
+using GeoSlicer.Utils;
+using GeoSlicer.Utils.Intersectors;
+using GeoSlicer.Utils.Intersectors.CoordinateComparators;
+
+namespace GeoSlicer.Tests.GridTests;
+
+public class CornerTests
+{
+    private static readonly double Epsilon = 1E-9;
+
+    private static readonly LineService LineService = new(Epsilon);
+
+    private static readonly GridSlicerHelper SlicerHelper =
+        new(new LinesIntersector(new EpsilonCoordinateComparator(Epsilon), LineService, Epsilon), LineService,
+            new EpsilonCoordinateComparator(), new ContainsChecker(LineService, Epsilon));
+
+    
+}
