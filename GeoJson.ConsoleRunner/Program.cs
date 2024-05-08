2556e83c28d308636d6a2f69d92907e48a5b4492
Daniil_Teslov <dteslov@gmail.com>
2024-05-08 12:13:13 +0600

    Changed Program for testing

--- GeoJson.ConsoleRunner/Program.cs
+++ GeoJson.ConsoleRunner/Program.cs
@@ -5,21 +5,24 @@ using GeoSlicer.Utils.PolygonClippingAlghorithm;
 using NetTopologySuite.Geometries;
 
 
-const double epsilon = 1E-15;
+const double epsilon = 1E-14;
 
 GeometryFactory gf =
     NetTopologySuite.NtsGeometryServices.Instance.CreateGeometryFactory(4326);
 
 LineService lineService = new LineService(epsilon);
-ICoordinateComparator coordinateComparator = new EpsilonCoordinateComparator(epsilon);
+ICoordinateComparator coordinateComparator = new EpsilonCoordinateComparator(1e-7);
 
 WeilerAthertonAlghorithm helper = new WeilerAthertonAlghorithm(new LinesIntersector(coordinateComparator, lineService, epsilon),
     lineService, coordinateComparator, new ContainsChecker(lineService, epsilon));
 
-LinearRing linearRing = new LinearRing(GeoJsonFileService.ReadGeometryFromFile<LineString>("TestFiles/part1.geojson.ignore").Coordinates);
+LinearRing linearRing = new LinearRing(GeoJsonFileService.ReadGeometryFromFile<LineString>("TestFiles/part2.geojson.ignore").Coordinates);
 Polygon polygon = GeoJsonFileService.ReadGeometryFromFile<Polygon>("TestFiles/source.geojson.ignore");
+if (!new TraverseDirection(lineService).IsClockwiseBypass(polygon.Shell)) new TraverseDirection(lineService).ChangeDirection(polygon.Shell);
 
-var result = helper.WeilerAtherton(polygon.Shell, linearRing);
+var result = new MultiPolygon(helper.WeilerAtherton(polygon.Shell, linearRing).Select(o => new Polygon(o)).ToArray());
+
+GeoJsonFileService.WriteGeometryToFile(result, "TestFiles\\bug_result_2.geojson.ignore");
 
 
 
